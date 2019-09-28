f5f1626562f6e67463527be805adf37ac4b0e608
AkshayKawde12 <51352659+AkshayKawde12@users.noreply.github.com>
2019-09-28 18:35:53 +0530

    Update NoteService.java

--- fundoo/src/main/java/com/bridgelabz/fundoo/notes/service/NoteService.java
+++ fundoo/src/main/java/com/bridgelabz/fundoo/notes/service/NoteService.java
@@ -1,38 +1,38 @@
-// package com.bridgelabz.fundoo.notes.service;
+package com.bridgelabz.fundoo.notes.service;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 
-// import com.bridgelabz.fundoo.notes.dto.NotesDto;
+import com.bridgelabz.fundoo.notes.dto.NotesDto;
 
-// import com.bridgelabz.fundoo.notes.model.Note;
-// import com.bridgelabz.fundoo.response.Response;
+import com.bridgelabz.fundoo.notes.model.Note;
+import com.bridgelabz.fundoo.response.Response;
 
-// public interface NoteService {
+public interface NoteService {
 
-// 	Response update(NotesDto notesDto, String token, Long noteId);
+	Response update(NotesDto notesDto, String token, Long noteId);
 
-// 	List<Note> getAllNotes(String token);
+	List<Note> getAllNotes(String token);
 
-// 	Response trash(String token, long noteId);
+	Response trash(String token, long noteId);
 
-// 	Response pin(String token, long noteId);
+	Response pin(String token, long noteId);
 
-// 	Response archive(String token, long noteId);
+	Response archive(String token, long noteId);
 
-// 	Response temporarydelete(String token, long noteId);
+	Response temporarydelete(String token, long noteId);
 
-// 	Response onCrete(NotesDto notesDto, String token);
+	Response onCrete(NotesDto notesDto, String token);
 
-// 	List<NotesDto> getAllArchive(String token);
+	List<NotesDto> getAllArchive(String token);
 
-// 	Response setColor(String token, long noteId, String color);
+	Response setColor(String token, long noteId, String color);
 
-// 	List<NotesDto> getAllTrash(String token);
+	List<NotesDto> getAllTrash(String token);
 
-// 	List<NotesDto> getAllPinedNotes(String token);
+	List<NotesDto> getAllPinedNotes(String token);
 
-// 	Response permanentdelete(String token, long noteId);
+	Response permanentdelete(String token, long noteId);
 
-// }
+}
